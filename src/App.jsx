042a4ade69938e6cb45ad6b16cc009240079c6b2
Majed AlFaraj <alfarajmajed4@gmail.com>
2026-05-07 14:57:52 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <html>
+        <div>
+          <h1>Music Web</h1>
+          <p>This proj.</p>
+        </div>
+      </html>
+  );
+}
+
+export default App;
